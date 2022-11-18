4c9bec5becbcc8f807c51558083594c95e3caa3b
Swagat Kumar Nayak <swagatkumar001@gmail.com>
2022-11-18 09:54:27 +0000

    making component interactive

--- src/components/Square/index.tsx
+++ src/components/Square/index.tsx
@@ -1,15 +1,18 @@
-import './Square.css';
+import { useState } from "react";
+import "./Square.css";
 
-interface SquareProps{
-  value: number,
+interface SquareProps {
+  value: number;
 }
 
-const Square = ({value}:SquareProps) => {
+const Square = ({ value }: SquareProps) => {
+  const [message, setMessage] = useState<string|null>(null);
+
   return (
-    <button className='square'>
-      {value}
+    <button className="square" onClick={() => setMessage('X')}>
+      {message}
     </button>
   );
-}
+};
 
 export default Square;
